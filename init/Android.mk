a207a1bca186557709598d2957845ce9bd49bad2
DJABHipHop <castellanos.kevin@rocketmail.com>
2019-05-22 10:01:16 -0400

    init_millet -> libinit_millet

--- init/Android.mk
+++ init/Android.mk
@@ -6,6 +6,6 @@ LOCAL_MODULE_TAGS := optional
 LOCAL_C_INCLUDES := system/core/init system/core/healthd bootable/recovery
 LOCAL_CFLAGS := -Wall
 LOCAL_SRC_FILES := init_millet.cpp
-LOCAL_MODULE := init_millet
+LOCAL_MODULE := libinit_millet
 
 include $(BUILD_STATIC_LIBRARY)
